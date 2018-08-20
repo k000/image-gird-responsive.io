1aa7f27aff5c29cb43f552949b361f3416dd7a4f
k000 <kintore@mineo.jp>
2018-08-20 09:52:26 +0900

    first

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function () {
+    var imgWidth = $(".img-wrap").width();
+    var imgheight = (imgWidth*0.7);
+    $(".img-wrap").css('height',imgheight);
+});
+$(window).resize(function () {
+    var imgWidth = $(".img-wrap").width();
+    var imgheight = (imgWidth*0.7);
+    $(".img-wrap").css('height',imgheight);
+});
+
+
+$(document).ready(function () {
+    var imgWidth = $(".img-wrap").width();
+    var imgheight = (imgWidth*0.7);
+    $(".img-wrap").css('height',imgheight);
+});
+$(window).resize(function () {
+    var imgWidth = $(".img-wrap").width();
+    var imgheight = (imgWidth*0.7);
+    $(".img-wrap").css('height',imgheight);
+});
